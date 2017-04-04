5ac672665f2ec9128442c9ddd18960a0a0ddf45e
崔永静 <cuiyongjing66@163.com>
2017-04-04 18:19:04 +0800

    implement is_member_of?(group)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
- belongs_to :group
+  belongs_to :group
 
- validates :content, presence: true
- scope :recent, -> { order("created_at DESC")}
+  validates :content, presence: true
+  scope :recent, -> { order("created_at DESC")}
 end
